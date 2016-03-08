3d7aba697a729c729a1de4f73bc8f40cb4277393
Destroyer69 <bagusinter@gmail.com>
2016-03-08 10:53:06 +0700

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>My Online Shop &trade;</title>
+		<script type="text/javascript" src="jquery.js"></script>
+		<link rel="stylesheet" href="includes/syeh.css"></link>
+	</head>
+	<body>
+		<div id="header"></div>
+		<div id="menu"></div>
+		<div id="kiri"></div>
+		<div id="kanan"></div>
+		<div id="footer"></div>
+	</body>
+</html>
